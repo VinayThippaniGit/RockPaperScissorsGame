165009da849cc4fd017fcb957e38eda59957c60e
VinayThippaniGit <165773410+VinayThippaniGit@users.noreply.github.com>
2024-06-22 22:29:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let computerMove = '';
+        let result = '';
+        function game(playerMove) {
+            let randomNumber = Math.random();
+            if (randomNumber > 0 && randomNumber < 1 / 3) {
+                computerMove = 'rock'
+            } else if (randomNumber > 0 && randomNumber < 2 / 3) {
+                computerMove = 'paper'
+            } else if (randomNumber > 0 && randomNumber < 1) {
+                computerMove = 'scissors'
+            }
+
+            if (playerMove === computerMove) {
+                result = 'Tie';
+            } else if ((playerMove === 'rock' && computerMove === 'scissors') ||
+                (playerMove === 'paper' && computerMove === 'rock') ||
+                (playerMove === 'scissors' && computerMove === 'paper')) {
+                result = 'You Win';
+            } else {
+                result = 'You loose';
+            }
+            document.getElementById('result').innerHTML = `<div class="computer">Computers Move : ${computerMove} </div> <div class='your'>Your's Move : ${playerMove} `;
+            document.getElementById('finalResult').innerHTML = result;
+            if (result === 'You Win') {
+                document.getElementById('finalResult').style.color = 'white';
+                document.getElementById('finalResult').style.backgroundColor='Green';
+            } else if (result === 'You loose') {
+                document.getElementById('finalResult').style.color = 'white';
+                document.getElementById('finalResult').style.backgroundColor='Red';
+            } else {
+                document.getElementById('finalResult').style.color = 'white';
+                document.getElementById('finalResult').style.backgroundColor='Orange';
+            }
+        }
